5ea5b487ccc5cbd596f12b44298f668c1cfc52a5
shanu17priya05 <shanu17priya05@gmail.com>
2026-03-17 22:19:10 +0530

    Service registry

---
+++ service-registry/src/test/java/com/corp/service_registry/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corp.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
